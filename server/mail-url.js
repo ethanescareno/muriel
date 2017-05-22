76d76a168fe39c5dee28e2f3e63a8b7d39c50731
Ethan Escareno Rosano <ethan.rosanoo@gmail.com>
2017-05-22 14:59:12 -0500

    stmp config

--- server/mail-url.js
+++ server/mail-url.js
@@ -2,9 +2,9 @@
 // process.env.MAIL_URL = "smtp://postmaster%40sandboxfde5267372b2461ebfa3c3f86efd100f.mailgun.org:27b6ad25bd62720b84b9092f26e45d9a@smtp.mailgun.org:587";
 Meteor.startup(function () {
   const smtp = {
-    username: 'recruiterq2017@gmail.com',
-    password: 'RecruiterQ1',
-    server: 'smtp.gmail.com',
+    username: 'postmaster@ridgetopat.com', //'recruiterq2017@gmail.com',
+    password: 'db87f5284d149f0f813bd499510e3de0', //'RecruiterQ1',
+    server: 'smtp.mailgun.org',//'smtp.gmail.com',
     port: 465
   }
   process.env.MAIL_URL = `smtp://${encodeURIComponent(smtp.username)}:${encodeURIComponent(smtp.password)}@${encodeURIComponent(smtp.server)}:${smtp.port}`;
