70981b11f4bc1aec76330fad09f7f725673523c7
Ethan Escareno Rosano <ethan.rosanoo@gmail.com>
2017-06-15 22:26:44 -0500

    Fix submit review button

--- server/email/config.js
+++ server/email/config.js
@@ -8,13 +8,13 @@ Meteor.startup(function() {
   Meteor.methods({
     'sendContactEmail': function(name, email, message) {
       this.unblock();
-
+      const url = Meteor.isProduction ? '67.205.180.89' : 'localhost:3000';
       Meteor.Mailgun.send({
         to: 'recipient@example.com',
         from: name + ' <' + email + '>',
         subject: 'New Contact Form Message',
         text: message,
-        html: Handlebars.templates['contactEmail']({siteURL: Meteor.absoluteUrl(), fromName: name, fromEmail: email, message: message})
+        html: Handlebars.templates['contactEmail']({siteURL: url, fromName: name, fromEmail: email, message: message})
       });
     }
   });
